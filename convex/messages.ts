b0c9927b9f8024c0a16cae8036032f0d0574b258
IsidroTrevino <A00837104@tec.mx>
2025-01-15 01:40:56 -0600

    Finished schema for handling different messages in conversations or dms

--- convex/messages.ts
+++ convex/messages.ts
@@ -14,7 +14,7 @@ export const create = mutation({
         workspaceId: v.id("workspaces"),
         channelId: v.optional(v.id("channels")),
         parentMessageId: v.optional(v.id("messages")),
-        // add conversation id
+        conversationId: v.optional(v.id("conversations")),
     },
     handler: async ( ctx, args ) => {
         const userId = await getAuthUserId(ctx);
@@ -29,13 +29,24 @@ export const create = mutation({
             throw new Error("Not a member of this workspace");
         }
 
-        //handle conversationId
+        let _conversationId = args.conversationId;
+
+        if(!args.conversationId && !args.channelId && args.parentMessageId) {
+            const parentMessage = await ctx.db.get(args.parentMessageId);
+
+            if (!parentMessage) {
+                throw new Error("Parent message not found");
+            }
+
+            _conversationId = parentMessage.conversationId;
+        }
 
         const messageId = await ctx.db.insert("messages", {
             memberId: member._id,
             body: args.body,
             image: args.image,
             channelId: args.channelId,
+            conversationId: _conversationId,
             workspaceId: args.workspaceId,
             parentMessageId: args.parentMessageId,
             updatedAt: Date.now(),
--- convex/schema.ts
+++ convex/schema.ts
@@ -24,6 +24,13 @@ const schema = defineSchema({
     })
     .index("by_workspace_id", ["workspaceId"]),
 
+    conversations: defineTable({
+        workspaceId: v.id("workspaces"),
+        memberOneId: v.id("members"),
+        memberTwoId: v.id("members"),
+    })
+    .index("by_workspace_id", ["workspaceId"]),
+    
     messages: defineTable({
         body: v.string(),
         image: v.optional(v.id("_storage")),
@@ -32,8 +39,23 @@ const schema = defineSchema({
         channelId: v.optional(v.id("channels")),
         parentMessageId: v.optional(v.id("messages")),
         updatedAt: v.number(),
-        //Add conversation ID
+        conversationId: v.optional(v.id("conversations")),
     })
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_member_id", ["memberId"])
+    .index("by_channel_id", ["channelId"])
+    .index("by_conversation_id", ["conversationId"])
+    .index("by_channel_id_parent_message_id_conversation_id", ["channelId", "parentMessageId", "conversationId"]),
+
+    reactions: defineTable({
+        workspaceId: v.id("workspaces"),
+        message: v.id("messages"),
+        memberId: v.id("members"),
+        value: v.string(),
+    })
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_message_id", ["message"])
+    .index("by_member_id", ["memberId"]),
 });
 
 export default schema;
